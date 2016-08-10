42bcba32448b523b146c6ee32704af98ca1f96bf
chengdanbiao <1204242970@QQ.com>
2016-08-11 00:07:26 +0800

    readfile and turn to dictionary Beta2.2

--- read.py
+++ read.py
@@ -4,10 +4,8 @@ while True:
         line = f.readline()
         if len(line) == 0:
                 break
-        print line,
-
+	
 	stin = line
 	i = stin.split(':')
-	print i
 	stinformation[i[0]] = i[1][:-1]
 print stinformation
