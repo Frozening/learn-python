c0b1c17812559dab42446c56d1d3e2a4251b86d2
chengdanbiao <1204242970@QQ.com>
2016-08-10 22:50:26 +0800

    readfile and turn to dictionary beta

--- read.py
+++ read.py
@@ -1,7 +1,13 @@
 f = file('score.txt','r')
 while True:
-	line = f.readline()
- 	if len(line) == 0:
-  		break
-	print line,
-f.close()
+        line = f.readline()
+        if len(line) == 0:
+                break
+        print line,
+
+	stin = line
+	i = stin.split(':')
+	print i
+	stinformation = {}
+	stinformation[i[0]] = i[1]
+  	print stinformation
