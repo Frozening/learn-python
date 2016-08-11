dd4cf94e329d0560572f22c50d1d7848e5c3b9df
chengdanbiao <1204242970@QQ.com>
2016-08-11 20:29:57 +0800

    file to dict and inqury system Beta1.0

--- read.py
+++ read.py
@@ -3,9 +3,15 @@ f = file('score.txt','r')
 while True:
         line = f.readline()
         if len(line) == 0:
-                break
-	
+                break	
 	stin = line
 	i = stin.split(':')
-	stinformation[i[0]] = i[1][:-1]
+	stinformation[i[0]] = tuple(i[1]i[:-1].split(','))
 print stinformation
+
+name = raw_input('Please enter a name:')
+if stinformation.has_key(name):
+	j = stinformation[name]
+	k = int(j[1])
+	l = int(j[2])
+	print "the student's sex,age and score are %s,%d and %d" %(j[0],k,l)
