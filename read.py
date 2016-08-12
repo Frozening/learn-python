184d2f749969d47b3cf1b2d75e1806028ffd325a
chengdanbiao <1204242970@QQ.com>
2016-08-12 17:46:40 +0800

    file to dict and inqury system

--- read.py
+++ read.py
@@ -1,4 +1,6 @@
 stinformation = {}
+compare = {}
+scores = []
 f = file('score.txt','r')
 while True:
         line = f.readline()
@@ -13,7 +15,12 @@ while True:
 	age = int(detail[1])
 	score = int(detail[2])
 	stinformation[name] = (sex,age,score)
-
+	compare[score] = (name,sex,age)
+	scores.append(score)
+Highest = scores[scores.index(max(scores))]
+Lowest = scores[scores.index(min(scores))]
+print "The Highest score is",Highest,"and the student's name,sex,age are %s,%s,%d" %(compare[Highest][0],compare[Highest][1],compare[Highest][2])
+print "The Lowsest score is",Lowest,"and the student's name,sex,age are %s,%s,%d" %(compare[Lowest][0],compare[Lowest][1],compare[Lowest][2])
 name = raw_input('Please enter a name:')
 if stinformation.has_key(name):
 	ziliao = stinformation[name]
