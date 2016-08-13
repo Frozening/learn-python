9e6f64c70942ed3b74793373c400748913df415e
chengdanbiao <1204242970@QQ.com>
2016-08-13 11:44:38 +0800

    file to dict and perfect inqury system

--- read.py
+++ read.py
@@ -17,10 +17,19 @@ while True:
 	stinformation[name] = (sex,age,score)
 	compare[score] = (name,sex,age)
 	scores.append(score)
-Highest = scores[scores.index(max(scores))]
-Lowest = scores[scores.index(min(scores))]
-print "The Highest score is",Highest,"and the student's name,sex,age are %s,%s,%d" %(compare[Highest][0],compare[Highest][1],compare[Highest][2])
-print "The Lowsest score is",Lowest,"and the student's name,sex,age are %s,%s,%d" %(compare[Lowest][0],compare[Lowest][1],compare[Lowest][2])
+max_key = 0
+min_key = 150
+for (key,value) in compare.items():
+	if key > max_key:
+		max_key = key
+	elif key < min_key:
+		min_key = key
+print "the Highest score : %d,%s,%s,%d" %(max_key,compare[max_key][0],compare[max_key][1],compare[max_key][2])
+print "the Lowest score : %d,%s,%s,%d" %(min_key,compare[min_key][0],compare[min_key][1],compare[min_key][2])
+#Another way: Highest = scores[scores.index(max(scores))]
+#Another way: Lowest = scores[scores.index(min(scores))]
+#Another way: print "The Highest score is",Highest,"and the student's name,sex,age are %s,%s,%d" %(compare[Highest][0],compare[Highest][1],compare[Highest][2])
+#print "The Lowsest score is",Lowest,"and the student's name,sex,age are %s,%s,%d" %(compare[Lowest][0],compare[Lowest][1],compare[Lowest][2])
 name = raw_input('Please enter a name:')
 if stinformation.has_key(name):
 	ziliao = stinformation[name]
